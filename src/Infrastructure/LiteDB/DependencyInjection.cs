4eaeb0f91a027e93081fe3823a0a57a4553fe8b6
Michal Gajda <michal@gajda.co.uk>
2024-03-05 13:43:11 +0000

    Add WebApi

--- src/Infrastructure/LiteDB/DependencyInjection.cs
+++ src/Infrastructure/LiteDB/DependencyInjection.cs
@@ -2,11 +2,15 @@ namespace Mulier.Infrastructure.LiteDB;
 
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Mulier.Domain.Interfaces;
+using Mulier.Infrastructure.LiteDB.Services;
 
 public static class DependencyInjection
 {
     public static IServiceCollection AddLiteDB(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddSingleton<IIngredientRepository, IngredientRepository>();
+
         return services;
     }
 }
---
+++ src/WebApi/Program.cs
@@ -0,0 +1,45 @@
+using Mulier.Application;
+using Mulier.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+await app.RunAsync();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
