95e50de69effc87f5c011a618fc7b2649771692d
Michal Gajda <michal@gajda.co.uk>
2024-03-06 21:58:43 +0000

    Add docker-compose.yml

--- src/Infrastructure/LiteDB/LiteDBOptions.cs
+++ src/Infrastructure/LiteDB/LiteDBOptions.cs
@@ -2,7 +2,7 @@ namespace Mulier.Infrastructure.LiteDB;
 
 public sealed record class LiteDBOptions
 {
-    public readonly static string SECTION = "LiteDB";
+    public static readonly string SectionName = "LiteDB";
     public string FileName { get; init; } = string.Empty;
 }
 
