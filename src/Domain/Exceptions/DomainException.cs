fc71fb3c89010ad529681e8da8ffcdfaf92266bb
Michal Gajda <michal@gajda.co.uk>
2024-03-25 12:36:17 +0000

    Code cleanup

--- src/Domain/Exceptions/DomainException.cs
+++ src/Domain/Exceptions/DomainException.cs
@@ -5,7 +5,7 @@ public abstract class DomainException : Exception
     public abstract string Code { get; }
     public Guid Id { get; protected set; } = Guid.Empty;
 
-    public DomainException(string message, Exception? innerException = default)
+    protected DomainException(string message, Exception? innerException = default)
     {
     }
 }
