862e8a68d03817c7ca0b95e462ac6f8d5861b10c
Michal Gajda <michal@gajda.co.uk>
2024-03-27 16:11:53 +0000

    Add ShoppingListItemDbEntity

--- src/Infrastructure/ShoppingLists/Interfaces/IShoppingListReadService.cs
+++ src/Infrastructure/ShoppingLists/Interfaces/IShoppingListReadService.cs
@@ -5,4 +5,5 @@ using Mulier.Infrastructure.ShoppingLists.Models;
 internal interface IShoppingListReadService
 {
     Task<IEnumerable<ShoppingListDbEntity>> GetShoppingLists(CancellationToken cancellationToken = default);
+    Task<IEnumerable<ShoppingListItemDbEntity>> GetShoppingListItems(ShoppingListId id, CancellationToken cancellationToken = default);
 }
---
+++ src/Infrastructure/ShoppingLists/Models/ShoppingListItemDbEntity.cs
@@ -0,0 +1,6 @@
+namespace Mulier.Infrastructure.ShoppingLists.Models;
+
+internal sealed class ShoppingListItemDbEntity
+{
+    public Guid Id { get; set; }
+}
