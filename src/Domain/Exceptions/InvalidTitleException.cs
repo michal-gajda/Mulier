1a071c7cb4c2f689e71a022f9830d9718aaa4fc9
Michal Gajda <michal@gajda.co.uk>
2024-03-28 11:47:30 +0000

    Code cleanup

--- src/Domain/Exceptions/InvalidTitleException.cs
+++ src/Domain/Exceptions/InvalidTitleException.cs
@@ -2,9 +2,9 @@
 
 public sealed class InvalidTitleException : DomainException
 {
-    public override string Code => DomainErrorCode.INVALID_TITLE;
-
     public InvalidTitleException(string title) : base($"Name '{title}' is not valid")
     {
     }
+
+    public override string Code => DomainErrorCode.INVALID_TITLE;
 }
--- src/Domain/Exceptions/ToDoNotFoundException.cs
+++ src/Domain/Exceptions/ToDoNotFoundException.cs
@@ -2,9 +2,9 @@ namespace Mulier.Domain.Exceptions;
 
 public sealed class ToDoNotFoundException : DomainException
 {
-    public override string Code => DomainErrorCode.NOT_FOUND;
-
     public ToDoNotFoundException(ToDoId id) : base($"ToDo '{id.Value}' not found")
     {
     }
+
+    public override string Code => DomainErrorCode.NOT_FOUND;
 }
