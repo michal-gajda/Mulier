64b56847fcd08cb1a98f2275a09e37970ec74d16
Michal Gajda <michal@gajda.co.uk>
2024-03-31 21:37:35 +0100

    Add Domain event

---
+++ src/Domain/Events/ShoppingListItemSelected.cs
@@ -0,0 +1,8 @@
+using MediatR;
+
+namespace Mulier.Domain.Events;
+
+public sealed record class ShoppingListItemSelected : INotification
+{
+    public required ShoppingListItemId Id { get; init; }
+}
