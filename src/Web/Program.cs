cff0ace0861f55f0cb103dc9d54191cb8e069938
Michal Gajda <michal@gajda.co.uk>
2024-04-15 15:31:29 +0100

    Add WAF

---
+++ src/Web/Program.cs
@@ -0,0 +1,11 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+var app = builder.Build();
+app.MapReverseProxy();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
