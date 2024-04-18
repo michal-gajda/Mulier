62fe91d5a418df1ddcad3170266014568b334785
Michal Gajda <michal@gajda.co.uk>
2024-04-18 19:42:54 +0100

    Migrate to sln in root folder

--- tests/Application.FunctionalTests/ToDos/Commands/CreateToDoTests.cs
+++ tests/Application.FunctionalTests/ToDos/Commands/CreateToDoTests.cs
@@ -1,20 +1,56 @@
-namespace Mulier.Application.FunctionalTests.ToDos.Commands;
+//namespace Mulier.Application.FunctionalTests.ToDos.Commands;
 
-using Mulier.Application.ToDos.Commands;
+//using Microsoft.Extensions.DependencyInjection;
+//using Mulier.Application.ToDos.Commands;
+//using Mulier.Domain.Interfaces;
+//using Mulier.Domain.Types;
+//using Mulier.Infrastructure;
 
-[TestClass]
-public sealed class CreateToDoTests
-{
-    private static readonly Guid ID = new("95310a20-73c8-4ba7-8b32-c886894a8d4c");
-    private static readonly string TITLE = "Title";
+//[TestClass]
+//public sealed class CreateToDoTests
+//{
+//    private static readonly Guid ID = new("95310a20-73c8-4ba7-8b32-c886894a8d4c");
+//    private static readonly string TITLE = "Title";
+//    private readonly IMediator mediator;
+//    private readonly ServiceProvider provider;
 
-    [TestMethod]
-    public void ShouldCreateToDo()
-    {
-        var command = new CreateToDo
-        {
-            Id = ID,
-            Title = TITLE,
-        };
-    }
-}
+//    public CreateToDoTests()
+//    {
+//        IServiceCollection services = new ServiceCollection();
+
+//        IConfiguration configuration = new ConfigurationBuilder()
+//            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
+//            .Build();
+
+//        services.AddSingleton(configuration);
+//        services.AddLogging();
+
+//        services.AddApplication();
+//        services.AddInfrastructure(configuration);
+
+//        this.provider = services.BuildServiceProvider();
+//        this.mediator = this.provider.GetRequiredService<IMediator>();
+//    }
+
+//    [TestMethod]
+//    public async Task ShouldCreateToDo()
+//    {
+//        var command = new CreateToDo
+//        {
+//            Id = ID,
+//            Title = TITLE,
+//        };
+
+//        await this.mediator.Send(command);
+
+//        var repository = this.provider.GetRequiredService<IToDoRepository>();
+
+//        var id = new ToDoId(ID);
+//        var sut = await repository.ReadAsync(id);
+
+//        sut.Should()
+//            .NotBeNull();
+//    }
+//}
+
+
