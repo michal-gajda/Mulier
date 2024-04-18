396fb1d175c9cae8705732e4bb53ef065c2ac300
Michal Gajda <michal@gajda.co.uk>
2024-04-18 06:27:33 +0100

    Add Blazor SPA hosting

---
+++ src/Shared/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Mulier.Shared;
+
+public class Class1
+{
+
+}
--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,11 +1,23 @@
+using Microsoft.AspNetCore.HttpLogging;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpLogging(options => options.LoggingFields = HttpLoggingFields.All);
+builder.Services.AddHealthChecks();
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
+
+app.UseHttpLogging();
+app.UseHealthChecks("/health");
+
 app.MapReverseProxy();
 
-app.MapGet("/", () => "Hello World!");
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
 
-app.Run();
+app.MapFallbackToFile("index.html");
+
+await app.RunAsync();
